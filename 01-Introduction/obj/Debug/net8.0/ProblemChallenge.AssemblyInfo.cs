9c237a61e3995e5b2de6a63d8de6c380b2bf5b4b
MERROUCHI Mouad <mouad02.merrouchi@gmail.com>
2024-03-05 11:28:30 +0100

    Robot on the moon final version

--- 01-Introduction/obj/Debug/net8.0/ProblemChallenge.AssemblyInfo.cs
+++ 01-Introduction/obj/Debug/net8.0/ProblemChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProblemChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6275db7bbe7adbb43991964ad77f9ec511d998b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52a24f003ab5a45afd8b5b653843716f2bd97a64")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProblemChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProblemChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
