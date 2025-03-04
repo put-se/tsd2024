5331d9831037f88dbe50874b220d86f97198f326
kasiarog <kasiarog2002@gmail.com>
2025-03-04 22:49:15 +0100

    Alarms task

--- 01-Introduction/Alarms.cs
+++ 01-Introduction/Alarms.cs
@@ -10,15 +10,31 @@ public class Alarms
 {
 	public int countAlarms(int[] volume, int S)
 	{
-        return default(int);
+		int counter = 0;
+		while (S > 0)
+		{
+			S -= volume[0];
+			int used_volume = volume[0];
+			for (int i = 0; i < volume.Length - 1; ++i)
+			{
+				volume[i] = volume[i + 1];
+			}
+			volume[volume.Length - 1] = used_volume;
+			counter += 1;
+		};
+
+		return counter;
 	}
 
 	#region Testing code
 	[STAThread]
-	private static Boolean KawigiEdit_RunTest(int testNum, int[] p0, int p1, Boolean hasAnswer, int p2) {
+	private static Boolean KawigiEdit_RunTest(int testNum, int[] p0, int p1, Boolean hasAnswer, int p2)
+	{
 		Console.Write("Test " + testNum + ": [" + "{");
-		for (int i = 0; p0.Length > i; ++i) {
-			if (i > 0) {
+		for (int i = 0; p0.Length > i; ++i)
+		{
+			if (i > 0)
+			{
 				Console.Write(",");
 			}
 			Console.Write(p0[i]);
@@ -34,67 +50,80 @@ public class Alarms
 		Boolean res;
 		res = true;
 		Console.WriteLine("Time: " + (endTime - startTime).TotalSeconds + " seconds");
-		if (hasAnswer) {
+		if (hasAnswer)
+		{
 			Console.WriteLine("Desired answer:");
 			Console.WriteLine("\t" + p2);
 		}
 		Console.WriteLine("Your answer:");
 		Console.WriteLine("\t" + answer);
-		if (hasAnswer) {
+		if (hasAnswer)
+		{
 			res = answer == p2;
 		}
-		if (!res) {
+		if (!res)
+		{
 			Console.WriteLine("DOESN'T MATCH!!!!");
-		} else if ((endTime - startTime).TotalSeconds >= 2) {
+		}
+		else if ((endTime - startTime).TotalSeconds >= 2)
+		{
 			Console.WriteLine("FAIL the timeout");
 			res = false;
-		} else if (hasAnswer) {
+		}
+		else if (hasAnswer)
+		{
 			Console.WriteLine("Match :-)");
-		} else {
+		}
+		else
+		{
 			Console.WriteLine("OK, but is it right?");
 		}
 		Console.WriteLine("");
 		return res;
 	}
-	public static void Run() {
+	public static void Main(string[] args)
+	{
 		Boolean all_right;
 		all_right = true;
-		
+
 		int[] p0;
 		int p1;
 		int p2;
-		
+
 		// ----- test 0 -----
-		p0 = new int[]{5,2,4};
+		p0 = new int[] { 5, 2, 4 };
 		p1 = 13;
 		p2 = 4;
 		all_right = KawigiEdit_RunTest(0, p0, p1, true, p2) && all_right;
 		// ------------------
-		
+
 		// ----- test 1 -----
-		p0 = new int[]{5,2,4};
+		p0 = new int[] { 5, 2, 4 };
 		p1 = 3;
 		p2 = 1;
 		all_right = KawigiEdit_RunTest(1, p0, p1, true, p2) && all_right;
 		// ------------------
-		
+
 		// ----- test 2 -----
-		p0 = new int[]{1};
+		p0 = new int[] { 1 };
 		p1 = 10000;
 		p2 = 10000;
 		all_right = KawigiEdit_RunTest(2, p0, p1, true, p2) && all_right;
 		// ------------------
-		
+
 		// ----- test 3 -----
-		p0 = new int[]{42,68,35,1,70,25,79,59,63,65,6,46,82,28,62,92,96,43,28,37,92,5,3,54,93,83,22,17,19,96,48,27,72,39,70,13,68,100,36,95,4,12,23,34,74,65,42,12,54,69};
+		p0 = new int[] { 42, 68, 35, 1, 70, 25, 79, 59, 63, 65, 6, 46, 82, 28, 62, 92, 96, 43, 28, 37, 92, 5, 3, 54, 93, 83, 22, 17, 19, 96, 48, 27, 72, 39, 70, 13, 68, 100, 36, 95, 4, 12, 23, 34, 74, 65, 42, 12, 54, 69 };
 		p1 = 9999;
 		p2 = 203;
 		all_right = KawigiEdit_RunTest(3, p0, p1, true, p2) && all_right;
 		// ------------------
-		
-		if (all_right) {
+
+		if (all_right)
+		{
 			Console.WriteLine("You're a stud (at least on the example cases)!");
-		} else {
+		}
+		else
+		{
 			Console.WriteLine("Some of the test cases had errors.");
 		}
 	}
