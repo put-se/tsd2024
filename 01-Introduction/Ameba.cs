42e79dc1d06180c80ca2d6641b35e618fc7b10c3
celiajosso <celia.josso@gmail.com>
2025-03-04 22:28:49 +0100

    ameba finished

--- 01-Introduction/Ameba.cs
+++ 01-Introduction/Ameba.cs
@@ -8,9 +8,13 @@ using System.Text.RegularExpressions;
 
 public class Ameba
 {
-	public int simulate(int[] X, int A)
-	{
-	    return default(int);
+	public int simulate(int[] X, int A) {
+	    for (int i = 0; i < X.Length; i++) {
+			if (X[i] == A) {
+				A *= 2;
+			}
+		} 
+		return A;
 	}
 
 	#region Testing code
