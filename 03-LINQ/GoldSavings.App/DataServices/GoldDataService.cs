bd397c4a45a22abf64e60a26069554cb6e3372df
skopczynska <sylwia.kopczynska@cs.put.poznan.pl>
2025-03-18 16:42:12 +0100

    task for 2025 edition

---
+++ 03-LINQ/GoldSavings.App/DataServices/GoldDataService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GoldSavings.App.Client;
+using GoldSavings.App.Model;
+
+namespace GoldSavings.App.Services
+{
+    public class GoldDataService
+    {
+        private readonly GoldClient _goldClient;
+
+        public GoldDataService()
+        {
+            _goldClient = new GoldClient();
+        }
+
+        public async Task<List<GoldPrice>> GetGoldPrices(DateTime startDate, DateTime endDate)
+        {
+            var prices = await _goldClient.GetGoldPrices(startDate, endDate);
+            return prices ?? new List<GoldPrice>();  // Prevent null values
+        }
+    }
+}
