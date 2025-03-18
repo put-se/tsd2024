5fa0f5a6f7ed40d9a6b34ac8311014983e6d6d3e
fquinaou <francois.quinaou@etudiant.univ-rennes1.fr>
2025-03-18 18:33:37 +0100

    init LINQ

--- 03-LINQ/GoldSavings.App/obj/Debug/net8.0/GoldSavings.App.AssemblyInfo.cs
+++ 03-LINQ/GoldSavings.App/obj/Debug/net8.0/GoldSavings.App.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ProblemChallenge")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("GoldSavings.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24ce3f0e7c548d44fe691090e1e2930e99ee91be")]
-[assembly: System.Reflection.AssemblyProductAttribute("ProblemChallenge")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ProblemChallenge")]
+[assembly: System.Reflection.AssemblyProductAttribute("GoldSavings.App")]
+[assembly: System.Reflection.AssemblyTitleAttribute("GoldSavings.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
